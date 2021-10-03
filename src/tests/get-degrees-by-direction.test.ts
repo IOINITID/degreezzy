2244de93016e43241bf0a42751f752a1422b654e
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-10-03 21:04:23 +0300

    Added tests for getDegreesByDirection function

--- src/tests/get-degrees-by-direction.test.ts
+++ src/tests/get-degrees-by-direction.test.ts
@@ -1,7 +1,15 @@
-import { getDegreesByDirection } from '../api/get-degrees-by-direction';
+import { getDegreesByDirection } from '../index';
 
-describe('Checks functions are available', () => {
+describe('Checks correct results', () => {
   it('Should have a getDegreesByDirection function', () => {
     expect(typeof getDegreesByDirection).toBe('function');
+  });
+
+  it('Should return correct degrees value', () => {
+    expect(getDegreesByDirection('N')).toBe(360);
+  });
+
+  it('Should return correct degrees value with rounding', () => {
+    expect(getDegreesByDirection('NNE', true)).toBe(23);
   });
 });
